902eaeb760084ce2e4f14a924c330d1343adf8a6
Dungar93 <dungarsoni1@gmail.com>
2026-01-14 02:05:48 +0530

    Add Pydantic and JSON output parsers for structured data generation

---
+++ OutPutParser/structured_output_schema.py
@@ -0,0 +1,27 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+prompt = PromptTemplate(
+    template="Give 3 facts about {topic} in JSON format.\n{format_instructions}",
+    input_variables=["topic"],
+    partial_variables={
+        "format_instructions": parser.get_format_instructions()
+    }
+)
+
+chain = prompt | model | parser
+
+print(chain.invoke({"topic": "black hole"}))
