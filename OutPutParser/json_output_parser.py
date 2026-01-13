4bf34d178894e20e2b75c27b4f5ec05f18d14a28
Dungar93 <dungarsoni1@gmail.com>
2026-01-14 01:06:45 +0530

    Add JSON and string output parsers with prompt templates for Hugging Face and OpenAI models

---
+++ OutPutParser/json_output_parser.py
@@ -0,0 +1,25 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+load_dotenv()
+
+
+parser  = JsonOutputParser()
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+model = ChatHuggingFace(llm=llm)
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
