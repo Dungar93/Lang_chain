6797be0545717afb4221a613303924d47c8b3dfc
Dungar93 <dungarsoni1@gmail.com>
2026-01-15 02:05:12 +0530

    runnable continue

--- Runnable/RunnablePassthrough.py
+++ Runnable/RunnablePassthrough.py
@@ -2,17 +2,12 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core import RunnableSequence, RunnableParallel
+from langchain_core import RunnableSequence, RunnableParallel, RunnablePassthrough
 
 load_dotenv()
 
 prompt1 = PromptTemplate(
-    template='Generate a tweet about {topic}',
-    input_variables=['topic']
-)
-
-prompt2 = PromptTemplate(
-    template='Generate a Linkedin post about {topic}',
+    template='Write a joke about {topic}',
     input_variables=['topic']
 )
 
@@ -20,12 +15,18 @@ model = ChatOpenAI()
 
 parser = StrOutputParser()
 
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
 parallel_chain = RunnableParallel({
-    'tweet': RunnableSequence(prompt1, model, parser),
-    'linkedin': RunnableSequence(prompt2, model, parser)
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
 })
 
-result = parallel_chain.invoke({'topic':'AI'})
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
-print(result['tweet'])
-print(result['linkedin'])
+print(final_chain.invoke({'topic':'cricket'}))
